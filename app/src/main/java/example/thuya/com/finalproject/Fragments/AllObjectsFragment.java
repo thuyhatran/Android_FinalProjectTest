44d154c8b967d5a0e7ef3e2665cee449fd50a9a8
thuya <thuyhatran@icloud.com>
2016-10-07 15:50:47 -0400

    Installed static list view for ListAllObjects and ListPickUpObjects

--- app/src/main/java/example/thuya/com/finalproject/Fragments/AllObjectsFragment.java
+++ app/src/main/java/example/thuya/com/finalproject/Fragments/AllObjectsFragment.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import example.thuya.com.finalproject.ObjectListAdapter;
 import example.thuya.com.finalproject.R;
 
 /**
@@ -13,9 +17,36 @@ import example.thuya.com.finalproject.R;
  */
 
 public class AllObjectsFragment extends Fragment {
-    @Override
+  /*  @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup containner, Bundle saveInstanceState){
         View rootView = inflater.inflate(R.layout.fragment_all_objects, containner, false);
+        return rootView;
+    }
+*/
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //STEP 1
+         final String[] OBJECTS_LIST =
+                new String[] { "image1", "image2", "image3", "image4"};
+
+        // ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        //STEP 2 : Adapter
+        //ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), R.layout.my_textview_container, R.id.textview_item, forecastArray);
+
+        ArrayAdapter   adapter = new ObjectListAdapter(getActivity().getApplicationContext(),OBJECTS_LIST);
+
+        //STEP 3
+        View rootView = inflater.inflate(R.layout.fragment_all_objects, container, false);
+
+        //STEP 4: listView
+        ListView list_view = (ListView) rootView.findViewById(R.id.listView_allObjects);
+
+        //STEP 5:
+        list_view.setAdapter(adapter);
+
         return rootView;
     }
 
--- app/src/main/java/example/thuya/com/finalproject/Fragments/PickUpObjectsFragment.java
+++ app/src/main/java/example/thuya/com/finalproject/Fragments/PickUpObjectsFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import example.thuya.com.finalproject.ObjectListAdapter;
 import example.thuya.com.finalproject.R;
 
 /**
@@ -15,7 +18,26 @@ import example.thuya.com.finalproject.R;
 public class PickUpObjectsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup containner, Bundle saveInstanceState){
+        //STEP 1
+        final String[] PICKUP_OBJECTS_LIST =
+                new String[] { "image1", "image2"};
+
+        // ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        //STEP 2 : Adapter
+        //ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), R.layout.my_textview_container, R.id.textview_item, forecastArray);
+
+        ArrayAdapter adapter = new ObjectListAdapter(getActivity().getApplicationContext(),PICKUP_OBJECTS_LIST);
+
+        //STEP 3
         View rootView = inflater.inflate(R.layout.fragment_pickup_objects, containner, false);
+
+        //STEP 4: listView
+        ListView list_view = (ListView) rootView.findViewById(R.id.listView_pickupObjects);
+
+        //STEP 5:
+        list_view.setAdapter(adapter);
+
         return rootView;
     }
 
